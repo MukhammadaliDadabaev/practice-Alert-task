851162ae7ebc477778c6e3d31d543d82e0e36452
Mukhammadali Dadabaev <92136722+MukhammadaliDadabaev@users.noreply.github.com>
2022-04-04 18:09:05 +0500

    Add  commit-example

--- src/components/App.js
+++ src/components/App.js
@@ -2,22 +2,26 @@ import React, { Component } from "react";
 
 class App extends Component {
   state = {
-    agreement: "",
+    agreement: false,
     email: "",
   };
 
   handelChecked = (e) => {
-    if (e.target.checked) {
+    if (e.target.name) {
       alert("👏 Congratulations!!!");
     } else if (this.state.name) {
       alert(this.state.email);
-    } else if (!this.state.checked) {
+    } else if (!this.state.name) {
       alert("Please accept our rules");
     } else {
       alert("This email is not valid");
     }
+    this.setState({
+      [e.target.name]: e.target.checked,
+    });
   };
   render() {
+    const { agreement } = this.state;
     return (
       <div className="wrapper">
         <div className="title">
@@ -26,7 +30,7 @@ class App extends Component {
         <form>
           <div className="form">
             <label htmlFor="email">Email</label>
-            <input type="email" name="email" id="email" />
+            <input type="email" name="email" id="email"/>
           </div>
           <br />
 
@@ -35,6 +39,7 @@ class App extends Component {
               type="checkbox"
               name="agreement"
               id="agreement"
+              value={agreement}
               onBlur={this.handelChecked}
             />
             <label htmlFor="agreement">Terms & Conditions</label>
